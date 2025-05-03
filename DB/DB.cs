a8304e37e7bf766e47587ce82fb195d9e4c0231b
devjunaeid <junaeidthamid@gmail.com>
2025-05-03 22:14:51 +0600

    Added: Subtraction Question generation section.

--- DB/DB.cs
+++ DB/DB.cs
@@ -10,6 +10,7 @@ internal class DB
     internal void INIT()
     {
         Random random = new Random();
+
         // Init 10 Addition Questions.
         for (int i = 0; i < 10; i++)
         {
@@ -29,10 +30,35 @@ internal class DB
                 }
                 else
                 {
-                    tempQ.Options.Add((option, $"[bold green]{random.Next(tempFirstNumber, 200)}[/]"));
+                    tempQ.Options.Add((option, $"[bold green]{random.Next(tempFirstNumber, tempFirstNumber + tempSecondNumber - 1)}[/]"));
                 }
             }
             QuestionsTable.Add(tempQ);
         }
+
+        // Init 10 Subtraction Questions.
+       for (int i = 0; i < 10; i++)
+        {
+            Questions tempQ = new Questions();
+            int tempFirstNumber = random.Next(1, 100); 
+            int tempSecondNumber = random.Next(tempFirstNumber, tempFirstNumber + 100);
+            tempQ.Question = $"What is [bold green]{tempSecondNumber}-{tempFirstNumber}[/] = ?";
+            tempQ.Type = QuestionType.Subtraction;
+            int ansRndSeed = random.Next(1, 4);
+            for (int j = 0; j < 4; j++)
+            {
+                Answer option = (Answer)j;
+                if (j == ansRndSeed)
+                {
+                    tempQ.Options.Add((option, $"[bold green]{tempSecondNumber - tempFirstNumber}[/]"));
+                    tempQ.SelectedAnswer = option;
+                }
+                else
+                {
+                    tempQ.Options.Add((option, $"[bold green]{random.Next(0, tempSecondNumber - tempFirstNumber - 1)}[/]"));
+                }
+            }
+            QuestionsTable.Add(tempQ);
+        } 
     }
 }
