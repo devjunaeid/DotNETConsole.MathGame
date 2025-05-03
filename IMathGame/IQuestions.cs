0bab41b18a98a1323681962ca7b0e4d52613a6e9
devjunaeid <junaeidthamid@gmail.com>
2025-05-03 15:33:30 +0600

    Removed: Variable from Interface.

--- IMathGame/IQuestions.cs
+++ IMathGame/IQuestions.cs
@@ -1,12 +1,6 @@
 namespace DotNETConsole.MathGame.IMathGame;
 
-using DotNETConsole.MathGame.Enums;
-
-internal interface IQuestions
+public interface IQuestions
 {
-    internal string Question { get; set; }
-    internal List<(Answer, string)> Options { get; set; }
-    internal QuestionType Type { get; set; }
-    internal Answer SelectedAnswer { get; set; }
-    internal DifficultyLevel Level { get; set; }
+    public bool AskQuestion();
 }
