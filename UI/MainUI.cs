0affa367a836c416e8c191791c205893e81b30d3
devjunaeid <junaeidthamid@gmail.com>
2025-05-04 14:59:22 +0600

    Added: Game menu controll & Game over message.

--- UI/MainUI.cs
+++ UI/MainUI.cs
@@ -7,11 +7,22 @@ using DotNETConsole.MathGame.Enums;
 internal class MainUI
 {
     static int score { get; set; } = 0;
-    static DB database {get; set;} = new DB();
-    internal void Welcome()
+    static int gameCycle { get; set; } = 0;
+    static DB database { get; set; } = new DB();
+    internal void GameInit()
     {
-        Console.WriteLine("Welcome!!!");
         database.INIT();
+    }
+
+    internal MainMenuChoices GameMenuController()
+    {
+        List<MainMenuChoices> options = new List<MainMenuChoices>((MainMenuChoices[])Enum.GetValues(typeof(MainMenuChoices)));
+        var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<MainMenuChoices>()
+                        .Title("Main Menu:")
+                        .AddChoices(options));
+
+        return choice;
     }
 
     // Game Type Selection.
@@ -20,7 +31,7 @@ internal class MainUI
         // Converting QuestionType Enum to List for Input Selection Choice.
         List<QuestionType> qType = new List<QuestionType>((QuestionType[])Enum.GetValues(typeof(QuestionType)));
 
-        
+
         var choice = AnsiConsole.Prompt(
             new SelectionPrompt<QuestionType>()
                 .Title("Which Mode Do You Like to Play?")
@@ -28,20 +39,59 @@ internal class MainUI
 
         return choice;
     }
-    internal void StartGame(QuestionType gameType) {
-        
+    internal void StartGame(QuestionType gameType)
+    {
+        //Reset Score.
+        score = 0;
         // Filtering Question from DB based on Game Type.
         List<Questions> filterdQuestions = database.QuestionsTable.Where(q => q.Type == gameType).ToList();
+        Scores scoreEntry = new Scores();
+        gameCycle += 1;
+        scoreEntry.Lebel = $"Game {gameCycle}";
+        int totalQuestions = filterdQuestions.Count;
+        int currentQuestion = 1;
 
         foreach (Questions q in filterdQuestions)
         {
-            Console.WriteLine($"Current Score: {score}");
+            string message = $"{scoreEntry.Lebel}:: Type-{gameType} :: Q({currentQuestion}/{totalQuestions}) --------- Current Score: {score}";
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth - message.Length) / 2, Console.WindowHeight / 10);
+            AnsiConsole.MarkupLine($"[bold blue]{message}[/]");
             bool correct = q.AskQuestion();
             if (correct)
             {
                 score += 5;
             }
+            currentQuestion++;
+            scoreEntry.UpdateScore(score);
             Console.Clear();
         }
+        database.ScoreTable.Add(scoreEntry);
+    }
+
+    internal void ShowScore()
+    {
+        var table = new Table();
+        table.AddColumns(new[] { "Entry", "DateTime", "Score" });
+
+        foreach (Scores sc in database.ScoreTable)
+        {
+            table.AddRow($"{sc.Lebel}", $"{sc.TimeStamp}", $"{score}");
+        }
+
+        AnsiConsole.Write(table);
+        AnsiConsole.MarkupLine($"[bold red]press 'Esc' to return to the main menu.[/]");
+    }
+
+    internal void GameEndMessage()
+    {
+        Console.Clear();
+        string message = $"Game Over! Your final score is: {score}";
+
+        //Write the message in the middle
+        Console.SetCursorPosition((Console.WindowWidth - message.Length) / 2, Console.WindowHeight / 2);
+        AnsiConsole.MarkupLine($"[bold green]{message}[/]");
+        Console.SetCursorPosition((Console.WindowWidth - message.Length) / 2, Console.WindowHeight / 2 + 10);
+        AnsiConsole.MarkupLine($"[bold red]press 'Esc' to return to the main menu.[/]");
     }
 }
