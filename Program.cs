827df01b48e8a48a3f6e95a0eb16c70af2b18be4
devjunaeid <junaeidthamid@gmail.com>
2025-05-04 15:30:21 +0600

    Implemeted game runtime controls.

--- Program.cs
+++ Program.cs
@@ -7,9 +7,46 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        MainUI GameUI = new MainUI();
-        GameUI.Welcome();
-        QuestionType gameType = GameUI.GameType();
-        GameUI.StartGame(gameType);
+        MainUI gameUI = new MainUI();
+        gameUI.GameInit();
+        bool gameRunning = true;
+        while (gameRunning)
+        {
+            gameUI.GameTopBar();
+            MainMenuChoices menuChoice = gameUI.GameMenuController();
+            switch (menuChoice)
+            {
+                case MainMenuChoices.StartGame:
+                    QuestionType gameType = gameUI.GameType();
+                    gameUI.StartGame(gameType);
+                    gameUI.GameEndMessage();
+                    while (true)
+                    {
+                        ConsoleKeyInfo keyInfo = Console.ReadKey();
+                        if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            break;
+                        }
+                    }
+                    Console.Clear();
+                    break;
+                case MainMenuChoices.ViewScore:
+                    gameUI.ShowScore();
+                    while (true)
+                    {
+                        ConsoleKeyInfo keyInfo = Console.ReadKey();
+                        if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            break;
+                        }
+                    }
+                    Console.Clear();
+                    break;
+                case MainMenuChoices.Exit:
+                    gameRunning = false;
+                    Console.Clear();
+                    break;
+            }
+        }
     }
 }
